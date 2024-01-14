a6704593bdd201fabe2ab191353a6676cfc6b1df
VictorECarvalho <victorelitt@gmail.com>
2024-01-14 19:59:45 -0300

    bins adjustment

--- integer_programming/bin_packing_formulation.jl
+++ integer_programming/bin_packing_formulation.jl
@@ -51,11 +51,24 @@ function main()
     parsed_args = parse_command_line()
 
     num_items, bins_capacity, items = get_instances(parsed_args["file_path"])
+    small = 0
+    for i in 1:num_items
+        if(items[i] <= bins_capacity/2)
+            small += 1
+        end
+    end
+    # arredonda small/2 para baixo
+    small = floor(Int64, small/2)
 
+
+    #num_bins = num_items
+    num_bins = num_items - small
+    println("num_bins: ", num_bins)
     model = Model(GLPK.Optimizer)
 
-    set_optimizer_attribute(model, "TimeLimitSec", parsed_args["time_limit"] * 60)
-    Random.seed!(parsed_args["seed"])
+   # set_optimizer_attribute(model, "TimeLimitSec", parsed_args["time_limit"] * 60)
+   # Random.seed!(parsed_args["seed"])
+   # set_optimizer_attribute(m, "msg_lev", GLPK.GLP_MSG_ALL)
 
     # Variables:
     # A binary matrix with dimensions N x N
@@ -66,25 +79,28 @@ function main()
     # 
     # The worst case is that we need one bin for each item, 
     # so we allocate space for a N x N matrix.
-    @variable(model, items_storage[1:num_items, 1:num_items], Bin)
+    @variable(model, items_storage[1:num_bins, 1:num_items], Bin)
 
     # An auxiliar vector to represent which bins are being used
-    @variable(model, bins_used[1:num_items], Bin)
+    @variable(model, bins_used[1:num_bins], Bin)
 
     # Constraints:
     # Each item must be store in exactly one bin
     for item in 1:num_items
-        @constraint(model, sum([items_storage[bin, item] for bin in 1:num_items]) == 1)
+        @constraint(model, sum([items_storage[bin, item] for bin in 1:num_bins]) == 1)
     end
 
     # The items stored in a bin must not exceed its capacity
-    for bin in 1:num_items
+    for bin in 1:num_bins
         @constraint(model, sum([items_storage[bin, item] * items[item] for item in 1:num_items]) <= bins_capacity * bins_used[bin])
     end
 
     # Populate the auxiliar vector
-    for bin in 1:num_items
-        @constraint(model, bins_used[bin] <= sum([items_storage[bin, item] for item in 1:num_items]))
+    for bin in 1:num_bins
+        #@constraint(model, bins_used[bin] <= sum([items_storage[bin, item] for item in 1:num_items]))
+        if(bin < num_bins)
+            @constraint(model, bins_used[bin+1] <= bins_used[bin]) 
+        end
         for item in 1:num_items
             @constraint(model, bins_used[bin] >= items_storage[bin, item])
         end
