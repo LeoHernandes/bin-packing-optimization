9998a10a6e6fc4d4e3d80deefce603bac1dde2d6
Vitor Caruso <crfvitor8d@hotmail.com>
2024-01-18 18:05:31 -0300

    Add time limit and deal with possible scenarios

--- integer_programming/bin_packing_formulation.jl
+++ integer_programming/bin_packing_formulation.jl
@@ -47,60 +47,69 @@ function get_instances(file_path)
     return num_itens, bins_capacity, itens
 end
 
-function main()
-    parsed_args = parse_command_line()
+function get_initial_num_bins(num_itens, bins_capacity, itens)
+    """
+    Returns the initial number of bins needed to store all the itens.
+    """
 
-    num_items, bins_capacity, items = get_instances(parsed_args["file_path"])
     small = 0
-    for i in 1:num_items
-        if(items[i] <= bins_capacity/2)
+    for i in 1:num_itens
+        if (itens[i] <= bins_capacity / 2)
             small += 1
         end
     end
-    # arredonda small/2 para baixo
-    small = floor(Int64, small/2)
 
+    small = floor(Int64, small / 2)
+    num_bins = num_itens - small
 
-    #num_bins = num_items
-    num_bins = num_items - small
-    println("num_bins: ", num_bins)
-    model = Model(GLPK.Optimizer)
+    return num_bins
+end
 
-    set_optimizer_attribute(model, "msg_lev", GLPK.GLP_MSG_ALL)
+function main()
+    parsed_args = parse_command_line()
+    num_itens, bins_capacity, itens = get_instances(parsed_args["file_path"])
+
+    num_bins = get_initial_num_bins(num_itens, bins_capacity, itens)
+    println("Starting num_bins: ", num_bins)
+
+    # tm_lim is in milliseconds
+    model = Model(optimizer_with_attributes(GLPK.Optimizer, "tm_lim" => (60000 * parsed_args["time_limit"])))
+
+    # set_optimizer_attribute(model, "msg_lev", GLPK.GLP_MSG_ALL)
     Random.seed!(parsed_args["seed"])
 
     # Variables:
     # A binary matrix with dimensions N x N
     # representing what bin is picked to store an item.
-    # N is the number of items. 
+    # N is the number of itens. 
     # The rows represents a bin.
     # The columns represents an item.
     # 
     # The worst case is that we need one bin for each item, 
     # so we allocate space for a N x N matrix.
-    @variable(model, items_storage[1:num_bins, 1:num_items], Bin)
+    @variable(model, itens_storage[1:num_bins, 1:num_itens], Bin)
 
     # An auxiliar vector to represent which bins are being used
     @variable(model, bins_used[1:num_bins], Bin)
 
     # Constraints:
     # Each item must be store in exactly one bin
-    for item in 1:num_items
-        @constraint(model, sum([items_storage[bin, item] for bin in 1:num_bins]) == 1)
+    for item in 1:num_itens
+        @constraint(model, sum([itens_storage[bin, item] for bin in 1:num_bins]) == 1)
     end
 
-    # The items stored in a bin must not exceed its capacity
+    # The itens stored in a bin must not exceed its capacity
     for bin in 1:num_bins
-        @constraint(model, sum([items_storage[bin, item] * items[item] for item in 1:num_items]) <= bins_capacity)
+        @constraint(model, sum([itens_storage[bin, item] * itens[item] for item in 1:num_itens]) <= bins_capacity)
     end
 
     # Populate the auxiliar vector
     for bin in 1:num_bins
-        if(bin < num_bins)
-            @constraint(model, bins_used[bin+1] <= bins_used[bin]) 
+        if (bin < num_bins)
+            @constraint(model, bins_used[bin+1] <= bins_used[bin])
         end
-        for item in 1:num_items
-            @constraint(model, bins_used[bin] >= items_storage[bin, item])
+        for item in 1:num_itens
+            @constraint(model, bins_used[bin] >= itens_storage[bin, item])
         end
     end
 
@@ -108,7 +117,20 @@ function main()
     @objective(model, Min, sum(bins_used))
 
     optimize!(model)
-    @show objective_value(model)
+
+    if termination_status(model) == OPTIMAL
+        println("Solution is optimal")
+    elseif termination_status(model) == TIME_LIMIT
+        if has_values(model)
+            println("Solution is suboptimal due to a time limit, but a primal solution is available")
+        else
+            println("The time limit was reached before finding a solution")
+        end
+    else
+        error("The model was not solved correctly.")
+    end
+    println(solution_summary(model))
+    @show(objective_value(model))
 end
 
 main()
