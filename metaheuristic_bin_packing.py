2c4ae57d4ef9ac3069a443bd819ba2038a0e8501
Vitor Caruso <crfvitor8d@hotmail.com>
2024-01-22 22:46:29 -0300

    Add python command line parsing and txt read

---
+++ metaheuristic_bin_packing.py
@@ -0,0 +1,45 @@
+import argparse
+
+
+def parse_command_line():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--seed", type=int, default=0, help="Random seed")
+    parser.add_argument(
+        "-t",
+        "--time_limit",
+        default=60,
+        type=int,
+        help="Program's time limit of execution in minutes",
+    )
+    parser.add_argument(
+        "file_path", help="Path of the txt file containing the instances"
+    )
+
+    return parser.parse_args()
+
+
+def get_instances(file_path):
+    """
+    Returns a 3-tuple that gets the following informations from the .txt file in file_path:
+    First line - num_itens
+    Second line - bins_capacity
+    Other lines - itens
+    """
+    with open(file_path, "r") as f:
+        num_itens = int(f.readline())
+        bins_capacity = int(f.readline())
+        itens = [int(weight) for weight in f]
+
+    return num_itens, bins_capacity, itens
+
+
+def main():
+    args = parse_command_line()
+    num_itens, bins_capacity, itens = get_instances(args.file_path)
+    print(args.time_limit, args.seed, args.file_path)
+    print("Num itens:", num_itens)
+    print("Bins capacity:", bins_capacity)
+    print("First five itens:", itens[:5])
+
+
+main()
