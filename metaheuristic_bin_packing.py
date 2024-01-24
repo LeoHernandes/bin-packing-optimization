b16684465e5897f2bf41a634862af25e5443b98c
Vítor Caruso <vitor.caruso@aprix.com.br>
2024-01-24 12:08:34 -0300

    Add taboo search draft and ideas

--- metaheuristic_bin_packing.py
+++ metaheuristic_bin_packing.py
@@ -1,4 +1,6 @@
 import argparse
+from time import time
+from typing import List, Tuple
 
 
 def parse_command_line():
@@ -33,20 +35,69 @@ def get_instances(file_path):
     return num_itens, bins_capacity, itens
 
 
+def get_bin(num_itens):
+    return [0] * num_itens
+
+
+# TODO: mudar essa função para retornar um vetor ao invés de uma matriz
+def get_starting_solution(itens, num_itens, bins_capacity):
+    total_sum = 0
+
+    bins = []
+    bins.append(get_bin(num_itens))
+
+    for i, item in enumerate(itens):
+        total_sum += item
+        if total_sum > bins_capacity:
+            total_sum = item
+            bins.append(get_bin(num_itens))
+        bins[-1][i] = 1
+
+    return bins
+
+
+class TabooMovement:
+    def __init__(self, current_bin, destination_bin, item_idx):
+        self.current_bin = current_bin
+        self.destination_bin = destination_bin
+        self.item_idx = item_idx
+
+    def __str__(self):
+        return f"[{self.item_idx}]: {self.current_bin} -> {self.destination_bin}"
+
+
+class TabooState:
+    pass
+
+
+def find_movements(bins: TabooState, bins_capacity: int) -> List[TabooMovement]:
+    pass
+
+
+"""
+TODO: 
+- Fazer a taboo list é um vetor em que cada posição diz a iteração que o item foi mexido, e comparar com o taboo tenure (len = num_itens)
+- Ao invés de utilizar a matriz binária, ser um vetor em que cada posição é a bin que o item está (len = num_itens)
+- Vetor com as capacidades de cada bin (len = num_bins)
+- Todos os itens podem mover para uma bin vazia que sempre existirá, a não ser que ele seja o único item na bin
+- Somente checamos o critério de aspiração por objetivo quando ele for o único item na bin 
+"""
+
+
+def taboo_search(bins: TabooState, bins_capacity: int) -> int:
+    TABOO_TENURE = len(bins) // 2
+    taboo_list = []
+
+    best_solution = len(bins)
+
+
 def main():
     args = parse_command_line()
     num_itens, bins_capacity, itens = get_instances(args.file_path)
-    print(args.time_limit, args.seed, args.file_path)
-    print("Num itens:", num_itens)
-    print("Bins capacity:", bins_capacity)
-    print("First five itens:", itens[:5])
-    num_bins = 0
-    total_sum = 0
-    for item in itens:
-        total_sum += item
-        if(total_sum > bins_capacity):
-            total_sum = item
-            num_bins +=1
+
+    bins = get_starting_solution(itens, num_itens, bins_capacity)
+
+    best_solution = taboo_search(bins, bins_capacity)
 
 
 main()
